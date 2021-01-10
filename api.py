7f8366d63fc7d994b5c9abf34193644464859bf7
reza-zoh <58261866+reza-zoh@users.noreply.github.com>
2021-01-10 17:19:45 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,24 +1,10 @@
 # Import libraries
 import io
 import pickle
-
 import pandas as pd
 from flask import Flask, request
 
 app = Flask(__name__)
-"""pour pouvoir y acceder partout sur internet """
-"run_with_ngrok(app)"
-
-
-@app.route('/predictor', methods=['POST'])
-def predictor():
-    model = pickle.load(open("final_prediction.pickle", "rb"))
-    req = io.StringIO(request.data.decode('utf-8'))
-    df = pd.read_csv(req, sep=',', header=0)
-    prediction = model.predict(df)
-    response = pd.DataFrame(prediction, index=df.index, columns=["The predicted decade of production"])
-    json_response = response.to_json(orient='table', indent=2)
-    return json_response, 201
 
 
 @app.route('/xgb', methods=['POST'])
